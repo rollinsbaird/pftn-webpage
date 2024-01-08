9ee72345d4c00dae6a6341f56452f7987caf5523
rollinsbaird <rmbaird@crimson.ua.edu>
2024-01-08 17:09:14 -0600

    Fix calcWeek

--- src/assets/calcWeek.js
+++ src/assets/calcWeek.js
@@ -1,9 +1,9 @@
 function calcWeek() {
-  const currentDate = new Date();
-  const startDate = new Date(currentDate.getFullYear(), 0, 1);
+  const currentDate = new Date(2024,);
+  const startDate = new Date(currentDate.getFullYear(), 0, 8);
   var days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
 
-  return (Math.ceil(days / 7) - 38) % 11;
+  return (Math.ceil(days / 7)) % 11;
 }
 
 export default calcWeek;
